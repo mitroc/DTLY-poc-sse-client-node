9769ef328b40e6e2e9c3bcf7910d84e14a3b5e4b
Michał Trocha <michal@dietly.pl>
2022-10-21 12:01:06 +0200

    move client files to this repo

---
+++ src/Columns.ts
@@ -0,0 +1,20 @@
+const columns = [
+  {
+    Header: "Origin",
+    accessor: "origin",
+  },
+  {
+    Header: "Flight",
+    accessor: "flight",
+  },
+  {
+    Header: "Arrival",
+    accessor: "arrival",
+  },
+  {
+    Header: "State",
+    accessor: "state",
+  },
+];
+
+export default columns;
